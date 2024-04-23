37229a2b273e2d9a3de2757e5148378e8584526f
Artiko F Samodra <48190353+artikosamodra@users.noreply.github.com>
2024-04-23 09:30:02 +0700

    Update calculateRank.js

--- src/calculateRank.js
+++ src/calculateRank.js
@@ -45,19 +45,22 @@ const calculateRank = ({
   //followers,
   prs,
   issues,
+  stargazers,
 }) => {
   const COMMITS_OFFSET = 1.65;
   const CONTRIBS_OFFSET = 1.65;
   const ISSUES_OFFSET = 1;
+  const STARS_OFFSET = 0.75;
   const PRS_OFFSET = 0.5;
-  //const FOLLOWERS_OFFSET = 0.45;
+  const FOLLOWERS_OFFSET = 0.45;
   const REPO_OFFSET = 1;
 
   const ALL_OFFSETS =
     CONTRIBS_OFFSET +
     ISSUES_OFFSET +
     PRS_OFFSET +
-    //FOLLOWERS_OFFSET +
+    STARS_OFFSET +
+    FOLLOWERS_OFFSET +
     REPO_OFFSET;
 
   const RANK_S_VALUE = 1;
@@ -78,6 +81,7 @@ const calculateRank = ({
     totalCommits * COMMITS_OFFSET +
     contributions * CONTRIBS_OFFSET +
     issues * ISSUES_OFFSET +
+    stargazers * STARS_OFFSET +
     prs * PRS_OFFSET +
     //followers * FOLLOWERS_OFFSET +
     totalRepos * REPO_OFFSET
